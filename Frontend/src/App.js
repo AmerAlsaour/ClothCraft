2840b2df804388ca2532e9d5691752a7f525b018
AmerAlsaour <amer.alsaoor@gmail.com>
2024-07-26 11:37:19 +0300

    land page login and regester front end

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
-
+import './Component/Style.css';
+import Login from './Pages/Log-in/Login.js'
+import Register from './Pages/Log-in/Register.js';
+import Homepage from './Hompage/Homepage.js';
+import { BrowserRouter as Router, Routes, Route , Navigate } from 'react-router-dom';
 function App() {
-  
   return (
-    <h1>asdf</h1>
+    <div className='App'>
+    <Router>
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path='/Login' element={<Login />} />
+            <Route path='/Register' element={<Register />} />
+          </Routes>
+    </Router>
+    </div>
+   
   );
 }
 
